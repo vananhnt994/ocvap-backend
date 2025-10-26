75f11788855d1782972e462bdb438e3ef2a8083a
vananhnt994 <49959187+vananhnt994@users.noreply.github.com>
2025-10-26 22:20:58 +0100

    first commit

---
+++ src/main/java/de/bht/app/ocvap/OcvapApplication.java
@@ -0,0 +1,13 @@
+package de.bht.app.ocvap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcvapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcvapApplication.class, args);
+	}
+
+}
